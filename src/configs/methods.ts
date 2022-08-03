85a5f70caa6dc9359cbf105c856b554c578681e4
haifeng_liu@bizconf.cn <haifeng_liu@bizconf.cn>
2022-08-03 10:56:04 +0800

    code update

--- src/configs/methods.ts
+++ src/configs/methods.ts
@@ -1,5 +1,5 @@
 /** 不需要登录就可以调用的method */
 export const freeMethods: Set<string> = new Set(['login', 'serviceList']);
 
-/** 必须登录才可以调用的method */
+/** 必须登录才可以调用的method，默认所有的method都需要登录才能调用 */
 export const authMethods: Set<string> = new Set([]);
